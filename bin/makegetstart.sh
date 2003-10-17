90444f45f5504f1018669fe2921346edf750d27d
mas <mas>
2003-10-17 14:55:45 +0000

    Finally fixed Unix shell script for PDF/HTML generation.

--- bin/makegetstart.sh
+++ bin/makegetstart.sh
@@ -19,20 +19,16 @@ pdflatex getstart
 
 # PDF generation is done now. I consider this as the most valuable because I
 # like reading printed manuals  ;-)
-
-# We're going to see that happens next.
-exit 0
-
 # Now deal with HTML generation for our nice web site. Start with Cleanup.
 # 
 rm *.aux getstart.css *.idx *.ind *.ilg *.log *.toc
 
-call htlatex getstart "html,2,info,next,sections+" %1 %2 %3 %4
-call htlatex getstart "html,2,info,next,sections+" %1 %2 %3 %4
-call htlatex getstart "html,2,info,next,sections+" %1 %2 %3 %4
-tex \def\filename{{getstart}{ind}{idx}{in}{out}} \input idxmake.4ht
+htlatex.sh getstart "html,2,info,next,sections+" %1 %2 %3 %4
+htlatex.sh getstart "html,2,info,next,sections+" %1 %2 %3 %4
+htlatex.sh getstart "html,2,info,next,sections+" %1 %2 %3 %4
+tex "\def\filename{{getstart}{ind}{idx}{in}{out}} \input" idxmake.4ht
 makeindex -o getstart.out getstart.in
-call htlatex getstart "html,2,info,next,sections+" %1 %2 %3 %4
+htlatex.sh getstart "html,2,info,next,sections+" %1 %2 %3 %4
 #
 rm -vf getstart.dvi *.4ct *.4tc *.aux *.bak *.idv *.idx *.ilg *.in *.ind \
 *.lg log *.out *.tid *.tmp *.toc *.xref
