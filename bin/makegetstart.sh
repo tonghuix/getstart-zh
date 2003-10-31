27617f8615615241393eb244ec85940f17160ed6
mas <mas>
2003-10-31 18:14:07 +0000

    Build dir tweaks.

--- bin/makegetstart.sh
+++ bin/makegetstart.sh
@@ -5,15 +5,15 @@
 
 cd `dirname $0`/..
 export BASEDIR=`pwd`
-mkdir build; cd build
+mkdir ${BASEDIR}/build; cd ${BASEDIR}/build
 cp -af ../source/*.tex ../source/*.eps .
 for i in *.eps; do convert -scale 800x600 $i `echo $i | sed -e 's/.eps\$/.jpg/g'`; done
 
 #
 # PDF first. Cleanup everything that is consideret not to be present here.
 #
-rm -vf *.pdf *.html *.4ct *.4tc *.aux *.bak *.idv *.idx *.ilg *.in *.ind \
-*.lg *.log *.out getstart.css *.tid *.tmp *.toc *.xref *.gif
+#rm -vf *.pdf *.html *.4ct *.4tc *.aux *.bak *.idv *.idx *.ilg *.in *.ind \
+#*.lg *.log *.out getstart.css *.tid *.tmp *.toc *.xref *.gif
 
 # Now we have to run 'pdflatex' several times - I'll figure out why (some
 # time ....).
@@ -23,6 +23,9 @@ pdflatex getstart
 pdflatex getstart
 makeindex getstart
 pdflatex getstart
+
+# Run the packaging script _now_ !
+rm -rf ${BASEDIR}/build
 
 # PDF generation is done now. I consider this as the most valuable because I
 # like reading printed manuals  ;-)
