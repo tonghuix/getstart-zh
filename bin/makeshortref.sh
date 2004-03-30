64748bcd4621dc523e1e4dfb3456d739f6186153
martin <martin>
2004-03-30 20:05:42 +0000

    First little test - and honour the fact that we should not build in other people's CVS repository  :-)

--- bin/makeshortref.sh
+++ bin/makeshortref.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 #
+cd `dirname $0`
+SRCDIR=`pwd`/../source; export SRCDIR
+# BE CAREFUL WITH THIS ONE !!!
+rm -r ${HOME}/getmp && cp -pr ${SRCDIR} ${HOME}/getmp
 
-rm -vf *.4ct *.4tc *.aux *.bak *.idv *.idx *.ilg *.in *.ind *.lg \
-*.FGShortRef.css *.log *.out *.tid *.tmp *.toc *.xref
+cd ${HOME}/getmp && rm -f *.4ct *.4tc *.aux *.bak *.idv *.idx *.ilg *.in \
+*.ind *.lg *.FGShortRef.css *.log *.out *.tid *.tmp *.toc *.xref
 
 pdflatex FGShortRef.tex
 pdflatex FGShortRef.tex
-rm -vf FGShortRef.dvi
+rm FGShortRef.dvi
 htlatex.sh FGShortRef %1 %2 %3 %4
 
-rm -vf FGShortRef.dvi *.4ct *.4tc *.aux *.bak *.idv *.idx *.ilg *.in *.ind \
+rm FGShortRef.dvi *.4ct *.4tc *.aux *.bak *.idv *.idx *.ilg *.in *.ind \
 *.*.lg log *.out *.tid *.tmp *.toc *.xref
-
